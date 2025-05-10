5d59a87bf278be743891fbb0afbcb5b091b4152a
itakuraharuna <itakuraharuna@dande-lion.co.jp>
2025-05-10 22:20:45 +0900

    課題4の実装

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
 import PostDetail from "./components/PostDetail/PostDetail";
+import Form from "./components/Form/Form";
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/posts/:id" element={<PostDetail />} />
+        <Route path="/contact" element={<Form />} />
       </Routes>
     </>
   );
